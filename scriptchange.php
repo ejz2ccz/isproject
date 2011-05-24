0b71a8a87b71d1248969bb8bb4a14d03d75b8108
Eli Zentman <ezentman@gmail.com>
2011-05-24 04:51:12 +0000

    updated scriptchange with new code

--- scriptchange.php
+++ scriptchange.php
@@ -1,15 +1,27 @@
+
 <html>
 <body>
 
 <?php
+$attributes = array();
 
-function script_change($script, $src)
-{
-	echo '<script type=' . $script . '  src= ' . $src . '></script>';
+$tag="form ";
+$attributes = array("action" => "some_action", "method" => "some_method", "name" => "somename",
+					"id" => "some_id", "event" => "some_event", "style" => "some_style", "class" => "some_class");
+$content="just say something";
+	echo tag($tag, $attributes, $content);
+function tag($tag, $attributes, $content){
+
+	$output='<' . $tag;
+	
+	foreach($attributes as $key=>$value){
+		$output .= '' . $key . '="' . $value . '" ';
+	}
+		$output .= '>' . $content . '</' . $tag . '>';
+	print_r($attributes);
+	return $output;
 }
-
-script_change("javascript","myfile");
-echo "script ran well";
 ?>
+</br>
 </body>
 </html>
